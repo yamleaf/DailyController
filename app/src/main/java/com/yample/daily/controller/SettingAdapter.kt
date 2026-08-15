2f6d977bc213242d4a61bd7a982180f2f2ffc317
yamleaf <li00ya@163.com>
2026-08-15 21:32:28 +0800

    fix: 补 SettingAdapter 圆点布局缺失的 LinearLayout import

--- app/src/main/java/com/yample/daily/controller/SettingAdapter.kt
+++ app/src/main/java/com/yample/daily/controller/SettingAdapter.kt
@@ -3,6 +3,7 @@ package com.yample.daily.controller
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
